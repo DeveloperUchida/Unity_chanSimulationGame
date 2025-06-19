d988d17fe251c5692f92ac09b7b8ff4a6e815966
DeveloperUchida <kouei2257@gmail.com>
2025-06-19 21:08:39 +0900

    UntiyGameMainのソースコードを一部変更

--- Assets/Scripts/GameMainScripts.cs
+++ Assets/Scripts/GameMainScripts.cs
@@ -1,14 +1,11 @@
-using System.Runtime.CompilerServices;
-using TMPro;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class GameMainScripts : MonoBehaviour
 {
-    //歩行スピード
-    private float WarkSppped = 4;
-    //回転スピード
-    private float RotateSpeed = 2;
+
+    private float WarkSppped = 4; //歩行スピード
+
+    private float RotateSpeed = 0.6f; //回転スピード
 
     // Start is called before the first frame update
     void Start()
@@ -21,15 +18,19 @@ public class GameMainScripts : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.W))
         {
-            transform.Translate(0, 0, WarkSppped);
+            transform.Translate(0, 0, WarkSppped * Time.deltaTime);
         }
         if (Input.GetKey(KeyCode.A))
         {
-            transform.Rotate(0, RotateSpeed, 0);
+            transform.Rotate(0, -RotateSpeed, 0);
         }
         if (Input.GetKey(KeyCode.S))
         {
-            transform.Translate(0, 0, -WarkSppped);
+            transform.Translate(0, 0, -WarkSppped * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(0, RotateSpeed, 0);
         }
     }
 }
