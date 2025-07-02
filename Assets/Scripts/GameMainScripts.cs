60eb85722a73a436369caa9c58ffd982530959da
DeveloperUchida <Kouei2257@gmail.com>
2025-07-02 14:54:04 +0900

    CodeUpdate

--- Assets/Scripts/GameMainScripts.cs
+++ Assets/Scripts/GameMainScripts.cs
@@ -7,30 +7,48 @@ public class GameMainScripts : MonoBehaviour
 
     private float RotateSpeed = 0.6f; //回転スピード
 
+    private Animator animator;
+    private bool isWorking = false; //歩行フラグ
     // Start is called before the first frame update
     void Start()
     {
-
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        bool moveKeyPressd = false;
+
         if (Input.GetKey(KeyCode.W))
         {
             transform.Translate(0, 0, WarkSppped * Time.deltaTime);
+            moveKeyPressd = true;
         }
         if (Input.GetKey(KeyCode.A))
         {
             transform.Rotate(0, -RotateSpeed, 0);
+            moveKeyPressd = true;
         }
         if (Input.GetKey(KeyCode.S))
         {
             transform.Translate(0, 0, -WarkSppped * Time.deltaTime);
+            moveKeyPressd = true;
         }
         if (Input.GetKey(KeyCode.D))
         {
             transform.Rotate(0, RotateSpeed, 0);
+            moveKeyPressd = true;
+        }
+
+        //アニメーション切り替え機能
+        if (moveKeyPressd && !isWorking)
+        {
+            WakingAnim();
+        }
+        else if (isWorking && isWorking)
+        {
+            stop();
         }
     }
 }
